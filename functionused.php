7ba47c51a37331ccac1406a9d1ee32a0669d3c73
khushiverma <44372851+Khushi-Verma@users.noreply.github.com>
2020-08-15 21:21:34 +0530

    functions of string

---
+++ functionused.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=<, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php>
+  //var_dump(): provide structured name
+  //print_r($variable name):
+   $string name="khushi";
+  var_dump(name):
+  print_r(name):
+  <?>
+</body>
+</html>
