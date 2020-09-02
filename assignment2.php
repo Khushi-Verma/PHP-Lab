25e30874fb1b43cafa02c3ad3aec6917b5dce3ac
khushiverma <44372851+Khushi-Verma@users.noreply.github.com>
2020-09-02 22:57:04 +0530

    Add files via upload

---
+++ assignment2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FirstPage</title>
+  <style>
+    *{
+      padding:0%;margin:0;
+      }
+      .heading{
+        margin-top:50px;
+        color: brown;
+        text-align: center;
+      }
+      .menu{
+        margin-top: 70px;
+        font-size:20px;
+        margin-left: 100px;
+        color: black;
+      }
+      body{
+        background-color: darkkhaki;
+      }
+  </style>
+</head>
+<body>
+<div class="heading">
+<h1>Conversion Assignment-2</h1>
+</div>
+<div class="menu">
+<ol>
+<li>Meters to Centi-Meters</li>
+<li>Kilometers to Inches</li>
+<li>Radian to Degree</li>
+<li>Temperature Conversion
+<ul><li>Celcius to Farenhite</li>
+<li>Farenhite to Celcius</li>
+</ul>
+</li>
+</ol>
+</div>
+  </body>
+</html>
